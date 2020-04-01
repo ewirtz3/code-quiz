30aa8b190ada876d3700a0acc057352853ac9184
Emily Wirtz <ewirtz3@gmail.com>
2020-04-01 16:58:58 -0500

    changed question answer to buttons

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+var startTime = document.querySelector("#start-time");
+var window = document.querySelector("#window");
+var startButton = document.querySelector("#start-button")
 //user clicks start -- addEventListener -- timer starts, first question appears
+startButton.addEventListener("click", function(event){
+    window.innerHTML = "";
+    
+})
 //questions -- addEventListener to all answer buttons, correct/incorrect both move to next question, correct displays Correct! in green at the bottom, incorrect displays Incorrect! in red at the bottom AND deducts 10 seconds from the timer
 //timer: counts down one second at a time UNLESS incorrect button is pressed
 //if timer runs out, questions stop and game end page shows
-//questions:
-//1: which of these is a CSS selector? a/element b/class c/id d/all of the above*
-//2: in HTML, the relationship between elements and their ancestor and descendant elements is known as what? a/family ties b/feudal system c/hierarchy d/adoption
-//3: a(n) _______ element's position is relative to its closest positioned parent element. a/relative b/absolute c/fixed d/scrolled
-//4: which of the following is an example of a string? a/'Spring is here!' b/Spring c/3 d/true
-//5: which of the following methods returns a random number between 9 and 1? a/Math.floor b/Math.random c/Math.ceil d/Math.length
+
 
