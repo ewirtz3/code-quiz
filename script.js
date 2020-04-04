5ba0735bfb739db873bb730198a4e6b47384be49
Emily Wirtz <ewirtz3@gmail.com>
2020-04-03 21:21:05 -0500

    adding startPage function

--- script.js
+++ script.js
@@ -8,57 +8,88 @@ var questions = [
     { question: 'In HTML, the relationship between elements and their ancestor and descendant elements is known as what?'},
     { question: 'The position of a(n) _______ element is relative to its closest positioned parent element.'},
     { question: 'Which of the following is an example of a string?'},
-    { question: 'Which of the following methods returns a random number between 9 and 1?'
-    }];
+    { question: 'Which of the following methods returns a random number between 9 and 1?'}
+    ];
 var answers = [
-    {   1: 'a) Element'
-        2: 'b) Class'
-        3: 'c) ID'
+    {   1: 'a) Element',
+        2: 'b) Class',
+        3: 'c) ID',
         4: 'd) All of the above'},
-    {   1: 'a) Family ties'
-        2: 'b) Feudal system'
-        3: 'c) Hierarchy'
+    {   1: 'a) Family ties',
+        2: 'b) Feudal system',
+        3: 'c) Hierarchy',
         4: 'd) Adoption'},
-    {   1: 'a) Relative'
-        2: 'b) Absolute'
-        3: 'c) Fixed'
+    {   1: 'a) Relative',
+        2: 'b) Absolute',
+        3: 'c) Fixed',
         4: 'd) Scrolled'},
-    {   1: 'a) Spring is here!'
-        2: 'b) 3'
-        3: 'c) true'
+    {   1: 'a) Spring is here!',
+        2: 'b) 3',
+        3: 'c) true',
         4: 'd) false'},
-    {   1: 'a) Math.floor'
-        2: 'b) Math.random'
-        3: 'c) Math.ceil'
+    {   1: 'a) Math.floor',
+        2: 'b) Math.random',
+        3: 'c) Math.ceil',
         4: 'd) Math.length'}];
 
 var correctAnswers = [4, 3, 2, 1, 2];
 var questionNum = 0;
 
+startPage();
+setTime();
+
+var startPage = function(){
+    var div = document.createElement("div");
+    div.setAttribute("class", "col-6 card border-primary mb-3");
+
+    var div2 = document.createElement("div");
+    div2.setAttribute("class", "card-header text-align-center");
+    div2.textContent = "Coding Quiz Challenge";
+
+    var div3 = document.createElement("div");
+    div3.setAttribute("class", "card-body text-primary");
+
+    var h4 = document.createElement("h4");
+    h4.textContent = "Try to answer the following code-related questions within the time given. Keep in mind that incorrect answers will subtract 10 seconds from your time!";
+
+    var button = document.createElement("button");
+    button.setAttribute("class", "btn btn-primary mb-3");
+    button.setAttribute("type", "button");
+    button.setAttribute("id", "start-button");
+    button.textContent = ("Start");
+
+    window.appendChild(div);
+    div.appendChild(div2);
+    div2.appendChild(div3);
+    div3.appendChild(h4);
+    div3.appendChild(button);
+};
 //user clicks start -- addEventListener -- timer starts, first question appears
 startButton.addEventListener("click", function(event){
     event.preventDefault();
     createCard();
 });
 
-//define var questionDiv and attach to div in html
-var questionDiv = document.querySelector("#questionDiv");
-
 //function to attach question and answer info to questionDiv
-function createCard(){
+var createCard = function(){
+    var div = document.createElement("div");
+    div.setAttribute("class", "col-6 card border-primary mb-3");
+
     var question = questions[i];
     question = document.createElement("div");
     question.setAttribute("class", "card-header text-align-center");
 
     var div2 = document.createElement("div");
-    div2.setAttribute ("class", "card-body text-primary");
+    div2.setAttribute("class", "card-body text-primary");
 
     var answer = answers[i];
     answer = document.createElement("button");
     answer.setAttribute("class", "btn btn-primary");
-    answer.setAttribute("type", button);
+    answer.setAttribute("type", "button");
 
-    questionDiv.appendChild(question);
+    window.appendChild(div);
+    div.appendChild(div2);
+    div.appendChild(question);
     div2.appendChild(answer);
 };
 
@@ -84,46 +115,33 @@ function renderTime(){
         window.innerHTML = "";
         window.value = endPage;
     }
-}
+};
 
-function startTimer () {
+function startTimer() {
     setTime();
     if (totalSeconds>0){
         interval = setInterval(function(){
             secondsElapsed++;
             renderTime();
         }, 1000);
-}
-
-
-function incorrectAns () {
-
-}
-correctAnswer.addEventListener("click", nextPage());
-incorrectAnswer.addEventListener("click", function(event){
-       
-    }
-
-
+};
 
 //if timer runs out, questions stop and game end page shows
 
 //game end page: final score is displayed and user enters their initials and clicks Submit. initials and score should go to local storage
 var submitScoreButton = document.querySelector(".submit");
-var initialsInput = document.querySelector("#initials");
-var finalScore = document.querySelector("#score");
+// var initialsInput = document.querySelector("#initials");
+// var finalScore = document.querySelector("#score");
 
-var user = {
-    initials: initialsInput.value.trim(),
-    score: finalScore.value
-};
-console.log(user);
+// var user = {
+//     initials: initialsInput.value.trim(),
+//     score: finalScore.value
+// };
+// console.log(user);
 
-//set new submission to storage upon click of Submit
-submitScoreButton.addEventListener("click", function(event){
+// set new submission to storage upon click of Submit
+submitScoreButton.addEventListener("click", function(event) {
     event.preventDefault();
     localStorage.setItem("user", JSON.stringify(user));
-    window.innerHTML = ""
-}
-
-//
+    window.innerHTML = "";
+})
