1ec6025a0c4cf276a9eb5aa8f7771e1452e1b92c
Emily Wirtz <ewirtz3@gmail.com>
2020-04-03 21:50:41 -0500

    rearranging js

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
 var startTime = document.querySelector("#start-time");
-var window = document.querySelector("#window");
-var startButton = document.querySelector("#start-button")
+var windowDiv = document.querySelector("#window");
 var endPage = document.querySelector("#game-end");
+
+startPage();
+setTime();
+
+function startPage(){
+    var div = document.createElement("div");
+    div.setAttribute("class", "col-6 card border-primary mb-3");
+
+    var div2 = document.createElement("div");
+    div2.setAttribute("class", "card-header text-align-center");
+    div2.textContent = "Coding Quiz Challenge";
+
+    var div3 = document.createElement("div");
+    div3.setAttribute("class", "card-body text-primary");
+
+    var h4 = document.createElement("h4");
+    h4.textContent = "Try to answer the following code-related questions within the time given. Keep in mind that incorrect answers will subtract 10 seconds from your time!";
+
+    var button = document.createElement("button");
+    button.setAttribute("class", "btn btn-primary mb-3");
+    button.setAttribute("type", "button");
+    button.setAttribute("id", "start-button");
+    button.textContent = ("Start");
+
+    windowDiv.appendChild(div);
+    div.appendChild(div2);
+    div2.appendChild(div3);
+    div3.appendChild(h4);
+    div3.appendChild(button);
+};
+
+//timer: counts down one second at a time UNLESS incorrect button is pressed
+var timer = document.querySelector("#start-time");
+var totalSeconds = 0;
+var secondsElapsed = 0;
+var interval;
+
+function setTime(){
+    clearInterval(interval);
+    totalSeconds = 90;
+};
+
+function renderTime(){
+    timer.textContent = totalSeconds - secondsElapsed;
+    if (secondsElapsed >= 90) {
+        windowDiv.innerHTML = "";
+        windowDiv.innerHTML = endPage;
+    }
+};
 
 var questions = [
     { question: 'Which of these is a CSS selector?'},
@@ -10,6 +58,7 @@ var questions = [
     { question: 'Which of the following is an example of a string?'},
     { question: 'Which of the following methods returns a random number between 9 and 1?'}
     ];
+
 var answers = [
     {   1: 'a) Element',
         2: 'b) Class',
@@ -30,48 +79,12 @@ var answers = [
     {   1: 'a) Math.floor',
         2: 'b) Math.random',
         3: 'c) Math.ceil',
-        4: 'd) Math.length'}];
-
-var correctAnswers = [4, 3, 2, 1, 2];
-var questionNum = 0;
-
-startPage();
-setTime();
-
-var startPage = function(){
-    var div = document.createElement("div");
-    div.setAttribute("class", "col-6 card border-primary mb-3");
-
-    var div2 = document.createElement("div");
-    div2.setAttribute("class", "card-header text-align-center");
-    div2.textContent = "Coding Quiz Challenge";
-
-    var div3 = document.createElement("div");
-    div3.setAttribute("class", "card-body text-primary");
-
-    var h4 = document.createElement("h4");
-    h4.textContent = "Try to answer the following code-related questions within the time given. Keep in mind that incorrect answers will subtract 10 seconds from your time!";
-
-    var button = document.createElement("button");
-    button.setAttribute("class", "btn btn-primary mb-3");
-    button.setAttribute("type", "button");
-    button.setAttribute("id", "start-button");
-    button.textContent = ("Start");
-
-    window.appendChild(div);
-    div.appendChild(div2);
-    div2.appendChild(div3);
-    div3.appendChild(h4);
-    div3.appendChild(button);
-};
-//user clicks start -- addEventListener -- timer starts, first question appears
-startButton.addEventListener("click", function(event){
-    event.preventDefault();
-    createCard();
-});
+        4: 'd) Math.length'}
+    ];
 
 //function to attach question and answer info to questionDiv
-var createCard = function(){
+createCard = function(){
+    for (var i = 0; i < questions.length; i++){
     var div = document.createElement("div");
     div.setAttribute("class", "col-6 card border-primary mb-3");
 
@@ -87,35 +100,11 @@ var createCard = function(){
     answer.setAttribute("class", "btn btn-primary");
     answer.setAttribute("type", "button");
 
-    window.appendChild(div);
+    windowDiv.appendChild(div);
     div.appendChild(div2);
     div.appendChild(question);
     div2.appendChild(answer);
-};
-
-window.innerHTML = "";
-    for (var i = 0; i < questions.length; i++){
-        window.innerHTML = questions[i];
-    }
-//questions -- addEventListener to all answer buttons, correct/incorrect both move to next question, correct displays Correct! in green at the bottom, incorrect displays Incorrect! in red at the bottom AND deducts 10 seconds from the timer
-//timer: counts down one second at a time UNLESS incorrect button is pressed
-var timer = document.querySelector("#start-time");
-var totalSeconds = 0;
-var secondsElapsed = 0;
-var interval;
-
-function setTime(){
-    clearInterval(interval);
-    totalSeconds = 90;
-};
-
-function renderTime(){
-    timer.textContent = 90;
-    if (secondsElapsed >= 90) {
-        window.innerHTML = "";
-        window.value = endPage;
-    }
-};
+}};
 
 function startTimer() {
     setTime();
@@ -125,6 +114,24 @@ function startTimer() {
             renderTime();
         }, 1000);
 };
+
+//user clicks start -- addEventListener -- timer starts, first question appears
+var startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", function(event){
+    event.preventDefault();
+    windowDiv.innerHTML ="";
+    createCard();
+    startTimer();
+});
+
+
+
+var correctAnswers = [4, 3, 2, 1, 2];
+var questionNum = 0;
+windowDiv.innerHTML = questions[i];
+    
+//questions -- addEventListener to all answer buttons, correct/incorrect both move to next question, correct displays Correct! in green at the bottom, incorrect displays Incorrect! in red at the bottom AND deducts 10 seconds from the timer
+
 
 //if timer runs out, questions stop and game end page shows
 
@@ -143,5 +150,5 @@ var submitScoreButton = document.querySelector(".submit");
 submitScoreButton.addEventListener("click", function(event) {
     event.preventDefault();
     localStorage.setItem("user", JSON.stringify(user));
-    window.innerHTML = "";
-})
+    windowDiv.innerHTML = "";
+})}
